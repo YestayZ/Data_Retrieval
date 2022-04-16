cbc607a11216a0662de9af0876f2afd34a5f298b
Yestay <yestay.z@gmail.com>
2022-04-17 01:13:39 +0500

    init

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -11,14 +11,9 @@ pprint(user_data)
 
 print('-------------------      ')
 
-import base64
+
 from github import Github
-from pprint import pprint
-# Github username
-#username = "YestayZ"
-# pygithub object
 g = Github()
-# get that user by username
 user = g.get_user(username)
 for repo in user.get_repos():
     print(repo)
